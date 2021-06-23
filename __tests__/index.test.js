5cf88b1e4ab0293c86468c1f0c010e93c5f8d814
Ivan Ulanovich <i.ulanovich@examus.net>
2021-06-23 10:13:32 +0500

    create test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,30 +1,34 @@
 // import nock from 'nock';
-// import fs from 'fs';
+import fs from 'fs';
 // import path from 'path';
 // import os from 'os';
 import pageLoader from '../src';
 
-const pathToFile = 'https://ru.hexlet.io/courses';
+const pathToPage = 'https://ru.hexlet.io/courses';
 const fileName = 'ru-hexlet-io-courses.html';
 
-beforeEach(async () => {
-  // nock(pathToFile)
-  //   .get('/')
-  //   .reply(200, 'HTML CONTENT');
+// beforeEach(async () => {
+//   const dtemp = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'), (_, dir) => {
+//     console.log(dir);
+//   });
+//   return dtemp;
+// });
 
-  // const pathToDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  // const rmdir = await fs.rmdir(pathToDir);
-  // const pathToLoadedFile = pageLoader(pathToFile);
-  // const res = await fs.readFile(pathToLoadedFile, 'utf-8');
-});
+// afterEach(() => {
+// removeCreatedFiles();
+// });
 
-test('default output dir', async () => {
-  const data = await pageLoader(pathToFile);
-  expect(data).toBe(`${process.cwd()}/${fileName}`);
-});
+// test('default output dir', async () => {
+//   const data = await pageLoader(pathToPage);
+//   const pathToFile = `${process.cwd()}/${fileName}`;
+//   expect(data).toBe(`${process.cwd()}/${fileName}`);
+//   fs.unlinkSync(pathToFile);
+// });
 
 test('specified output dir', async () => {
-  const outputDir = '/var/tmp';
-  const data = await pageLoader(pathToFile, outputDir);
-  expect(data).toBe(`${outputDir}/${fileName}`);
+  const outputDir = '/home/ulanivan';
+  const pathToFile = `${outputDir}/${fileName}`;
+  const result = await pageLoader(pathToPage, outputDir);
+  fs.unlinkSync(result);
+  expect(result).toBe(pathToFile);
 });
